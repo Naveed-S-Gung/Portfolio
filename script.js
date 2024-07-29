64bb44b36904de8c757aef7c1b41d5c50695a193
T1IG3ER <naveedsohailg@gmail.com>
2024-07-29 11:55:21 +0300

    required

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const hamburger = document.getElementById('hamburger'); 
-const menu = document.querySelector('.menu'); 
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const menu = document.getElementById('menu');
 
-hamburger.addEventListener('click', function () { 
-	const hamIcon = this.querySelector('.hamburger-icon'); 
-	const crossIcon = this.querySelector('.cross-icon'); 
-	if (hamIcon.style.display === "none") { 
-		hamIcon.style.display = "inline-block"
-		menu.style.display = "none"
-		crossIcon.style.display = "none"
-	} 
-	else { 
-		crossIcon.style.display = "inline-block"
-		hamIcon.style.display = "none"
-		menu.style.display = "block"
-	} 
+    function toggleMenu() {
+        hamburger.classList.toggle('open');
+        menu.classList.toggle('show');
+    }
+
+    hamburger.addEventListener('click', toggleMenu);
+
+    document.addEventListener('click', function (e) {
+        if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
+            hamburger.classList.remove('open');
+            menu.classList.remove('show');
+        }
+    });
 });
+
+
+let lastScrollTop = 0;
 window.addEventListener('scroll', function() {
-  var navbar = document.getElementById('.navbar'); // Replace 'navbar' with the ID of your navbar element
-  if (window.scrollY > 0) {
-    navbar.style.display = 'none';
-  } else {
-    navbar.style.display = 'block';
-  }
+    const header = document.querySelector('.header');
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+    lastScrollTop = scrollTop;
 });
-
